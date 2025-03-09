7ce49f58951abfeb75deaf7e904ea15557e244e0
李娜(Allen) <allenAip@gmail.com>
2025-03-09 18:56:42 +0800

    feat:新增 PromptTemplateTest 测试类以验证文本模板格式化功能

---
+++ alinesno-smart-model-adapter-core/src/test/java/com/agentsflex/core/test/PromptTemplateTest.java
@@ -0,0 +1,21 @@
+package com.agentsflex.core.test;
+
+import com.agentsflex.core.prompt.template.TextPromptTemplate;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PromptTemplateTest {
+
+    @Test
+    public void test001(){
+        Map<String ,Object> map = new HashMap<>();
+        map.put("x","abc");
+        TextPromptTemplate promptTemplate = TextPromptTemplate.create("你好，{ } 今天是星期 :{x}{ x }--xx--{x }---{  x }--}}--{ x} }");
+        String string = promptTemplate
+            .format(map).toString();
+        System.out.println(string);
+
+    }
+}
