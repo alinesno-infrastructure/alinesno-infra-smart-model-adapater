7b2c819da8ff1fe89efb89a83b04c1979d77d837
林晓(Lily) <lilyAip@gmail.com>
2025-03-11 00:12:47 +0800

    feat:添加 SynthesizeSpeechRequest 类，并引入 Lombok 注解

--- alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/speech/SynthesizeSpeechRequest.java
+++ alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/speech/SynthesizeSpeechRequest.java
@@ -1,4 +1,13 @@
 package com.agentsflex.core.speech;
 
-public class SynthesizeSpeechRequest {
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SynthesizeSpeechRequest extends BaseSpeechRequest {
+
+    private String text ;
+    private String voice ; // male female
+
 }
