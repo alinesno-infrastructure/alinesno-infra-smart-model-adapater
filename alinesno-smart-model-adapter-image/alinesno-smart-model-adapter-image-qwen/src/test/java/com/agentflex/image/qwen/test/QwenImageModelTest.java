ce2d899c4ae26109d67c4a3a0120e0c65b141b19
李娜(Allen) <allenAip@gmail.com>
2025-03-11 00:12:47 +0800

    feat:新增 QwenImageModelTest 测试类以验证图像生成功能

---
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-qwen/src/test/java/com/agentflex/image/qwen/test/QwenImageModelTest.java
@@ -0,0 +1,40 @@
+package com.agentflex.image.qwen.test;
+
+import com.agentsflex.core.image.GenerateImageRequest;
+import com.agentsflex.core.image.Image;
+import com.agentsflex.core.image.ImageResponse;
+import com.agentsflex.image.qwen.QwenImageModel;
+import com.agentsflex.image.qwen.QwenImageModelConfig;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.UUID;
+
+public class QwenImageModelTest {
+
+    @Test
+    public void testGenImage(){
+        QwenImageModelConfig config = new QwenImageModelConfig();
+        config.setApiKey(System.getenv("ALINESNO_QIWEN_API_KEY")) ;
+
+        QwenImageModel openAiImageModel = new QwenImageModel(config);
+
+        GenerateImageRequest request = new GenerateImageRequest();
+        request.setPrompt("一间有着精致窗户的花店，漂亮的木质门，摆放着花朵");
+        request.setNegativePrompt("人物");
+
+        request.setN(3);
+
+        request.setSize(512,512);
+        ImageResponse generate = openAiImageModel.generate(request);
+        if (generate != null && generate.getImages() != null){
+            int index = 0;
+            for (Image image : generate.getImages()) {
+                image.writeToFile(new File("/Users/luodong/Desktop/demo-images/image_" + UUID.randomUUID() + "_" +(index++)+".jpg"));
+            }
+        }
+
+        System.out.println(generate);
+    }
+
+}
