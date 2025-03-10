39776a7cc777584b53716d85c159b611c01e5ea2
林晓(Lily) <lilyAip@gmail.com>
2025-03-11 00:12:47 +0800

    feat:新增DoubaoSpeechModel类，实现语音识别和合成功能

---
+++ alinesno-smart-model-adapter-speech/alinesno-smart-model-adapter-speech-doubao/src/main/java/com/agentsflex/speech/doubao/DoubaoSpeechModel.java
@@ -0,0 +1,21 @@
+package com.agentsflex.speech.doubao;
+
+import com.agentsflex.core.speech.RecognizeSpeechRequest;
+import com.agentsflex.core.speech.SpeechModel;
+import com.agentsflex.core.speech.SpeechResponse;
+import com.agentsflex.core.speech.SynthesizeSpeechRequest;
+
+/**
+ * Doubao语音识别接口
+ */
+public class DoubaoSpeechModel implements SpeechModel {
+    @Override
+    public SpeechResponse synthesize(SynthesizeSpeechRequest request) {
+        return null;
+    }
+
+    @Override
+    public SpeechResponse recognize(RecognizeSpeechRequest request) {
+        return null;
+    }
+}
