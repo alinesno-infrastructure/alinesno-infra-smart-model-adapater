19c26593f1cbc1d4058a202fb199c32304b7e8d6
林晓(Lily) <lilyAip@gmail.com>
2025-03-11 00:12:47 +0800

    feat:新增ImageUrl类，包含图片URL属性

---
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-qwen/src/main/java/com/agentsflex/image/qwen/bean/ImageUrl.java
@@ -0,0 +1,11 @@
+package com.agentsflex.image.qwen.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// 图片 URL 类
+@Data
+@AllArgsConstructor
+public class ImageUrl {
+    private String url;
+}
