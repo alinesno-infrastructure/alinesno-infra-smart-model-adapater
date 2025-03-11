de230664bc271bca1b135bc8242d737fbc21f9f2
梁明树(Nina) <ninaAip@gmail.com>
2025-03-12 00:12:17 +0800

    feat:新增 QwenSpeechModelTest 测试用例以支持语音识别功能

--- alinesno-smart-model-adapter-speech/alinesno-smart-model-adapter-speech-qwen/src/test/java/com/agentsflex/speech/qwen/test/QwenSpeechModelTest.java
+++ alinesno-smart-model-adapter-speech/alinesno-smart-model-adapter-speech-qwen/src/test/java/com/agentsflex/speech/qwen/test/QwenSpeechModelTest.java
@@ -1,5 +1,6 @@
 package com.agentsflex.speech.qwen.test;
 
+import com.agentsflex.core.speech.RecognizeSpeechRequest;
 import com.agentsflex.core.speech.SpeechResponse;
 import com.agentsflex.core.speech.SynthesizeSpeechRequest;
 import com.agentsflex.speech.qwen.QwenSpeechModel;
@@ -7,6 +8,7 @@ import com.agentsflex.speech.qwen.QwenSpeechModelConfig;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.List;
 import java.util.UUID;
 
 public class QwenSpeechModelTest {
@@ -22,6 +24,25 @@ public class QwenSpeechModelTest {
         request.setText("一间有着精致窗户的花店，漂亮的木质门，摆放着花朵");
 
         SpeechResponse generate = openAiSpeechModel.synthesize(request);
+        if (generate != null && generate.getSpeechMp3() != null){
+            generate.getSpeechMp3().writeToFile(new File("E:\\tmp\\mp3\\Speech_" + UUID.randomUUID() + ".mp3"));
+        }
+
+        System.out.println(generate);
+    }
+
+    @Test
+    public void testRecognize(){
+        QwenSpeechModelConfig config = new QwenSpeechModelConfig();
+        config.setApiKey(System.getenv("ALINESNO_QIWEN_API_KEY")) ;
+        config.setModel("sensevoice-v1");
+
+        QwenSpeechModel openAiSpeechModel = new QwenSpeechModel(config);
+
+        RecognizeSpeechRequest request = new RecognizeSpeechRequest();
+        request.setAudioList(List.of("http://data.linesno.com/ddaiai_whisper.wav" , "https://dashscope.oss-cn-beijing.aliyuncs.com/samples/audio/sensevoice/rich_text_example_1.wav")) ;
+
+        SpeechResponse generate = openAiSpeechModel.recognize(request);
         if (generate != null && generate.getSpeechMp3() != null){
             generate.getSpeechMp3().writeToFile(new File("E:\\tmp\\mp3\\Speech_" + UUID.randomUUID() + ".mp3"));
         }
