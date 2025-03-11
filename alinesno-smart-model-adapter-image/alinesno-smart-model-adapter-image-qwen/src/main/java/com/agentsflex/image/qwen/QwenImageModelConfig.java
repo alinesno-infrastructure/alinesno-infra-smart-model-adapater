65a74765d33d71b52e64d56990bb5df5d06e7840
梁明树(Nina) <ninaAip@gmail.com>
2025-03-12 00:12:17 +0800

    feat:新增 understandEndpoint 配置项，用于支持图像理解相关接口。

--- alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-qwen/src/main/java/com/agentsflex/image/qwen/QwenImageModelConfig.java
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-qwen/src/main/java/com/agentsflex/image/qwen/QwenImageModelConfig.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class QwenImageModelConfig implements Serializable {
     private String endpoint = "https://dashscope.aliyuncs.com/api/v1/services/aigc/text2image/image-synthesis";
+    private String understandEndpoint = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions" ;
     private String taskEndpoint = "https://dashscope.aliyuncs.com/api/v1/tasks/";
     private String model = QwenImageModels.wanx2_0_t2i_turbo;
     private String apiKey;
