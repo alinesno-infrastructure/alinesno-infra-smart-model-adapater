c6ec7f8b47345299af04b3d8d26cbec5965968cc
王磊(Levi) <leviAip@gmail.com>
2025-03-16 12:55:46 +0800

    test: 添加 GiteeAITest 测试用例并优化代码结构

--- alinesno-smart-model-adapter-llm/alinesno-smart-model-adapter-llm-gitee/src/test/java/com/agentsflex/llm/gitee/GiteeAITest.java
+++ alinesno-smart-model-adapter-llm/alinesno-smart-model-adapter-llm-gitee/src/test/java/com/agentsflex/llm/gitee/GiteeAITest.java
@@ -1,24 +1,38 @@
 package com.agentsflex.llm.gitee;
 
 import com.agentsflex.core.llm.response.AiMessageResponse;
+import com.agentsflex.core.message.AiMessage;
 import com.agentsflex.core.prompt.FunctionPrompt;
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 public class GiteeAITest {
 
+    @SneakyThrows
     public static void main(String[] args) {
         GiteeAiLlmConfig config = new GiteeAiLlmConfig();
-        config.setApiKey("your-api-key");
+        config.setApiKey(System.getenv("ALINESNO_GITEE_API_KEY"));
+        config.setModel("DeepSeek-R1");  // TODO fix:处理和优化推理模型适配
 
         GiteeAiLlm llm = new GiteeAiLlm(config);
-        String result = llm.chat("你好");
-        System.out.println(result);
+//        String result = llm.chat("你好");
+//        System.out.println(result);
+
+        llm.chatStream("9.10和9.9哪个更大", (context, response) -> {
+            AiMessage message = response.getMessage();
+
+            System.out.println(">>>> 推理内容: " + message.getReasoningContent());
+            System.out.println(">>>> 结果内容: " + message.getFullContent());
+
+        });
+
+        Thread.sleep(50 * 1000);
     }
 
     @Test
     public void testFunctionCalling() {
         GiteeAiLlmConfig config = new GiteeAiLlmConfig();
-        config.setApiKey("your-api-key");
+        config.setApiKey(System.getenv("ALINESNO_GITEE_API_KEY"));
         config.setModel("Qwen2.5-72B-Instruct");
 
         GiteeAiLlm llm = new GiteeAiLlm(config);
