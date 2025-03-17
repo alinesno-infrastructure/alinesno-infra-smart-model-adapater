16be05cfa0a2e04ca8ed9eea2b5862ce8971ea9b
李娜(Allen) <allenAip@gmail.com>
2025-03-18 00:13:23 +0800

    feat:新增 SiliconflowLlmConfig 类，封装硅流模型配置参数。

---
+++ alinesno-smart-model-adapter-llm/alinesno-smart-model-adapter-llm-siliconflow/src/main/java/com/agentsflex/llm/siliconflow/SiliconflowLlmConfig.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2023-2025, alinesno-smart-model-adapter (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.llm.siliconflow;
+
+import com.agentsflex.core.llm.LlmConfig;
+import lombok.Getter;
+
+@Getter
+public class SiliconflowLlmConfig extends LlmConfig {
+
+    private static final String DEFAULT_MODEL = "qwen-turbo";
+    private static final String DEFAULT_ENDPOINT = "https://api.siliconflow.cn";
+    private String defaultEmbeddingModel = "text-embedding-v1";
+
+    public SiliconflowLlmConfig() {
+        setEndpoint(DEFAULT_ENDPOINT);
+        setModel(DEFAULT_MODEL);
+    }
+
+    public void setDefaultEmbeddingModel(String defaultEmbeddingModel) {
+        this.defaultEmbeddingModel = defaultEmbeddingModel;
+    }
+}
