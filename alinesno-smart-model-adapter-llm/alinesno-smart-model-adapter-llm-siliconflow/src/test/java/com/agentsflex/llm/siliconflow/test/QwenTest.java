7ba68493cbfc6d51b57df1d9a08e533582ab868a
李娜(Allen) <allenAip@gmail.com>
2025-03-18 00:13:23 +0800

    feat: 添加 QwenTest 测试类，包含聊天流测试、函数调用测试及嵌入向量测试功能。

---
+++ alinesno-smart-model-adapter-llm/alinesno-smart-model-adapter-llm-siliconflow/src/test/java/com/agentsflex/llm/siliconflow/test/QwenTest.java
@@ -0,0 +1,62 @@
+package com.agentsflex.llm.siliconflow.test;
+
+import com.agentsflex.core.document.Document;
+import com.agentsflex.core.llm.Llm;
+import com.agentsflex.core.llm.response.AiMessageResponse;
+import com.agentsflex.core.store.VectorData;
+import com.agentsflex.llm.siliconflow.QwenLlm;
+import com.agentsflex.llm.siliconflow.QwenLlmConfig;
+import com.agentsflex.core.message.AiMessage;
+import com.agentsflex.core.prompt.FunctionPrompt;
+import org.junit.Test;
+
+public class QwenTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        QwenLlmConfig config = new QwenLlmConfig();
+
+        //https://bailian.console.aliyun.com/?apiKey=1#/api-key
+        // config.setApiKey("sk-28a6be3236****");
+
+        config.setApiKey(System.getenv("ALINESNO_QIWEN_API_KEY")) ;
+        config.setModel("qwen-plus");
+
+        Llm llm = new QwenLlm(config);
+        llm.chatStream("请写一个小兔子战胜大灰狼的故事", (context, response) -> {
+            AiMessage message = response.getMessage();
+            System.out.println(">>>> " + message);
+        });
+
+        Thread.sleep(10000);
+    }
+
+
+    @Test
+    public void testFunctionCalling() throws InterruptedException {
+        QwenLlmConfig config = new QwenLlmConfig();
+        config.setApiKey(System.getenv("ALINESNO_QIWEN_API_KEY")) ;
+        config.setModel("qwen-turbo");
+
+        Llm llm = new QwenLlm(config);
+
+        FunctionPrompt prompt = new FunctionPrompt("今天北京的天气怎么样", WeatherFunctions.class);
+        AiMessageResponse response = llm.chat(prompt);
+
+        System.out.println(response.callFunctions());
+        // "Today it will be dull and overcast in 北京"
+    }
+
+
+    @Test
+    public void testEmbedding() throws InterruptedException {
+        QwenLlmConfig config = new QwenLlmConfig();
+        config.setApiKey(System.getenv("ALINESNO_QIWEN_API_KEY")) ;
+        config.setModel("qwen-turbo");
+
+        Llm llm = new QwenLlm(config);
+        VectorData vectorData = llm.embed(Document.of("test"));
+
+
+        System.out.println(vectorData);
+    }
+}
