4d754a4524f9dd525faef81ae4c2017cc92a626e
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 00:14:07 +0800

    feat:新增 QwenVision 类，实现基于 Qwen 的视觉模型适配功能。

---
+++ alinesno-smart-model-adapter-vision/alinesno-smart-model-adapter-vision-qwen/src/main/java/com/agentflex/vision/qwen/QwenVision.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright (c) 2023-2025, alinesno-smart-model-adapter (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentflex.vision.qwen;
+
+import com.agentsflex.core.llm.ChatOptions;
+import com.agentsflex.core.llm.StreamResponseListener;
+import com.agentsflex.core.llm.client.HttpClient;
+import com.agentsflex.core.llm.client.LlmClient;
+import com.agentsflex.core.llm.client.LlmClientListener;
+import com.agentsflex.core.llm.client.impl.SseClient;
+import com.agentsflex.core.llm.response.AiMessageResponse;
+import com.agentsflex.core.parser.AiMessageParser;
+import com.agentsflex.core.prompt.Prompt;
+import com.agentsflex.core.util.StringUtil;
+import com.agentsflex.core.vision.BaseVision;
+import com.agentsflex.core.vision.BaseVisionClientListener;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QwenVision extends BaseVision<QwenVisionConfig> {
+
+
+    HttpClient httpClient = new HttpClient();
+
+    public AiMessageParser aiMessageParser = QwenVisionUtil.getAiMessageParser(false);
+    public AiMessageParser streamMessageParser = QwenVisionUtil.getAiMessageParser(true);
+
+    public QwenVision(QwenVisionConfig config) {
+        super(config);
+    }
+
+
+    @Override
+    public AiMessageResponse chat(Prompt prompt, ChatOptions options) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer " + getConfig().getApiKey());
+
+
+        String payload = QwenVisionUtil.promptToPayload(prompt, config, options, false);
+        String endpoint = config.getEndpoint();
+        String response = httpClient.post(endpoint + "/compatible-mode/v1/chat/completions", headers, payload);
+
+        if (config.isDebug()) {
+            System.out.println(">>>>receive payload:" + response);
+        }
+
+        if (StringUtil.noText(response)) {
+            return AiMessageResponse.error(prompt, response, "no content for response.");
+        }
+
+        JSONObject jsonObject = JSON.parseObject(response);
+        JSONObject error = jsonObject.getJSONObject("error");
+
+        AiMessageResponse messageResponse = new AiMessageResponse(prompt, response, aiMessageParser.parse(jsonObject));
+
+        if (error != null && !error.isEmpty()) {
+            messageResponse.setError(true);
+            messageResponse.setErrorMessage(error.getString("message"));
+            messageResponse.setErrorType(error.getString("type"));
+            messageResponse.setErrorCode(error.getString("code"));
+        }
+
+        return messageResponse;
+    }
+
+
+    @Override
+    public void chatStream(Prompt prompt, StreamResponseListener listener, ChatOptions options) {
+        LlmClient llmClient = new SseClient();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer " + getConfig().getApiKey());
+        headers.put("X-DashScope-SSE", "enable"); //stream
+
+        String payload = QwenVisionUtil.promptToPayload(prompt, config, options, true);
+        LlmClientListener clientListener = new BaseVisionClientListener(this, llmClient, listener, prompt, streamMessageParser);
+
+        String endpoint = config.getEndpoint();
+        llmClient.start(endpoint + "/compatible-mode/v1/chat/completions", headers, payload, clientListener, config);
+    }
+
+}
