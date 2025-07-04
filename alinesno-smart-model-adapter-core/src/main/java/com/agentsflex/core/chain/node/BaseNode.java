2669e9f91788ee20598357e134550afddf591094
周健(James) <jamesAip@gmail.com>
2025-07-05 06:07:11 +0800

    refactor:重构BaseNode类，使用Parameter替代InputParameter和OutputKey

--- alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/chain/node/BaseNode.java
+++ alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/chain/node/BaseNode.java
@@ -15,92 +15,52 @@
  */
 package com.agentsflex.core.chain.node;
 
-import com.agentsflex.core.chain.*;
-import com.agentsflex.core.util.StringUtil;
+import com.agentsflex.core.chain.ChainNode;
+import com.agentsflex.core.chain.Parameter;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public abstract class BaseNode extends ChainNode {
 
-    protected String description;
-    protected List<InputParameter> inputParameters;
-    protected List<OutputKey> outputKeys;
+    protected List<Parameter> parameters;
+    protected List<Parameter> outputDefs;
 
-
-    public String getDescription() {
-        return description;
+    public void setParameters(List<Parameter> parameters) {
+        this.parameters = parameters;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public List<Parameter> getParameters() {
+        return parameters;
     }
 
-    public List<InputParameter> getInputParameters() {
-        return inputParameters;
-    }
-
-    public void setInputParameters(List<InputParameter> inputParameters) {
-        this.inputParameters = inputParameters;
-    }
-
-    public void addInputParameter(InputParameter inputParameter) {
-        if (inputParameters == null) {
-            inputParameters = new java.util.ArrayList<>();
+    public void addInputParameter(Parameter parameter) {
+        if (parameters == null) {
+            parameters = new java.util.ArrayList<>();
         }
-        inputParameters.add(inputParameter);
+        parameters.add(parameter);
     }
 
-    public List<OutputKey> getOutputKeys() {
-        return outputKeys;
+
+    public List<Parameter> getOutputDefs() {
+        return outputDefs;
     }
 
-    public void setOutputKeys(List<OutputKey> outputKeys) {
-        this.outputKeys = outputKeys;
+    public void setOutputDefs(List<Parameter> outputDefs) {
+        this.outputDefs = outputDefs;
     }
 
-    public void addOutputKey(OutputKey outputKey) {
-        if (outputKeys == null) {
-            outputKeys = new java.util.ArrayList<>();
+    public void addOutputDef(Parameter parameter) {
+        if (outputDefs == null) {
+            outputDefs = new java.util.ArrayList<>();
         }
-        outputKeys.add(outputKey);
+        outputDefs.add(parameter);
     }
 
-
-    public Map<String, Object> getChainParameters(Chain chain, List<InputParameter> inputParameters) {
-        Map<String, Object> variables = new HashMap<>();
-        if (inputParameters != null) {
-            for (InputParameter parameter : inputParameters) {
-                RefType refType = parameter.getRefType();
-                Object value;
-                if (refType == RefType.INPUT) {
-                    value = parameter.getRef();
-                } else if (refType == RefType.REF) {
-                    value = chain.get(parameter.getRef());
-                } else {
-                    value = chain.get(parameter.getName());
-                }
-                if (parameter.isRequired() &&
-                    (value == null || (value instanceof String && StringUtil.noText((String) value)))) {
-                    chain.stopError(this.getName() + " Missing required parameter:" + parameter.getName());
-                }
-                if (value == null || value instanceof String) {
-                    value = value == null ? "" : ((String) value).trim();
-                    if (parameter.getDataType() == DataType.Boolean) {
-                        value = "true".equalsIgnoreCase((String) value) || "1".equalsIgnoreCase((String) value);
-                    } else if (parameter.getDataType() == DataType.Number) {
-                        value = Long.parseLong((String) value);
-                    }
-                }
-
-                variables.put(parameter.getName(), value);
-            }
+    public void addOutputDefs(Collection<Parameter> parameters) {
+        if (outputDefs == null) {
+            outputDefs = new java.util.ArrayList<>();
         }
-        return variables;
-    }
-
-    public Map<String, Object> getParameters(Chain chain) {
-        return getChainParameters(chain, this.inputParameters);
+        outputDefs.addAll(parameters);
     }
 }
