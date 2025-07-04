db4e525cd36e195dcdf1467d239443078ca864ee
周健(James) <jamesAip@gmail.com>
2025-07-05 06:07:12 +0800

    test:新增PromptTemplate测试用例，覆盖多种模板格式和默认值情况。

--- alinesno-smart-model-adapter-core/src/test/java/com/agentsflex/core/test/PromptTemplateTest.java
+++ alinesno-smart-model-adapter-core/src/test/java/com/agentsflex/core/test/PromptTemplateTest.java
@@ -9,13 +9,42 @@ import java.util.Map;
 public class PromptTemplateTest {
 
     @Test
-    public void test001(){
-        Map<String ,Object> map = new HashMap<>();
-        map.put("x","abc");
-        TextPromptTemplate promptTemplate = TextPromptTemplate.create("你好，{ } 今天是星期 :{x}{ x }--xx--{x }---{  x }--}}--{ x} }");
+    public void test001() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("x", "abc");
+        TextPromptTemplate promptTemplate = TextPromptTemplate.of("你好，{}  今天是星期 :{{x}}{{ x }}--xx--{{x }---{{  x }}--}}--{ x} }");
         String string = promptTemplate
             .format(map).toString();
         System.out.println(string);
 
+    }
+
+    @Test
+    public void test002() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("useName", "Michael");
+        map.put("aaa", "星期3");
+        TextPromptTemplate promptTemplate = TextPromptTemplate.of("你好，{{  useName }}  今天是星期 :{{aaa   }}}----- {a}aa");
+        String string = promptTemplate.formatToString(map);
+        System.out.println(string);
+    }
+
+
+    @Test
+    public void test003() {
+        String templateStr = "你好 {{ user.name ?? '匿名' }}，欢迎来到 {{ site ?? 'AgentsFlex.com' }}！";
+        TextPromptTemplate template = new TextPromptTemplate(templateStr);
+
+        Map<String, Object> params = new HashMap<>();
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "Michael");
+        params.put("user", user);
+        params.put("site", "AIFlowy.tech");
+
+        System.out.println(template.format(params));
+        // 输出：你好 Michael，欢迎来到 AIFlowy.tech！
+
+        System.out.println(template.format(new HashMap<>()));
+        // 输出：你好 匿名，欢迎来到 AgentsFlex.com！
     }
 }
