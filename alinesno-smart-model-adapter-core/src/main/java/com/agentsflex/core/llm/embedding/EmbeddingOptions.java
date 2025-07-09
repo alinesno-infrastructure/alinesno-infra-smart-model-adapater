d00e282c90bab006601f6a585e177280c07e8b37
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:10 +0800

    feat:创建EmbeddingOptions类，包含模型设置和默认实例

---
+++ alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/llm/embedding/EmbeddingOptions.java
@@ -0,0 +1,32 @@
+package com.agentsflex.core.llm.embedding;
+
+import com.agentsflex.core.util.StringUtil;
+import lombok.Getter;
+
+@Getter
+public class EmbeddingOptions {
+    public static final EmbeddingOptions DEFAULT = new EmbeddingOptions(){
+        @Override
+        public void setModel(String model) {
+            throw new IllegalStateException("Can not set modal to the default instance.");
+        }
+    };
+
+    private String model;
+
+    public String getModelOrDefault(String defaultModel) {
+        return StringUtil.noText(model) ? defaultModel : model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    @Override
+    public String toString() {
+        return "EmbeddingOptions{" +
+            "model='" + model + '\'' +
+            '}';
+    }
+}
