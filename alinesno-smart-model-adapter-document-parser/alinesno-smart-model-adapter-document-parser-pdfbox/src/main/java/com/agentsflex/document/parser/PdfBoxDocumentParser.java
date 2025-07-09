c37225d628405b75c3dd9985a4b8fae7da9d31bf
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:09 +0800

    feat:实现PdfBoxDocumentParser类，用于解析PDF文件内容

---
+++ alinesno-smart-model-adapter-document-parser/alinesno-smart-model-adapter-document-parser-pdfbox/src/main/java/com/agentsflex/document/parser/PdfBoxDocumentParser.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2023-2025, alinesno-smart-model-adapter (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.document.parser;
+
+import com.agentsflex.core.document.Document;
+import com.agentsflex.core.document.DocumentParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class PdfBoxDocumentParser implements DocumentParser {
+    @Override
+    public Document parse(InputStream stream) {
+        try (PDDocument pdfDocument = PDDocument.load(stream)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(pdfDocument);
+            return new Document(text);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
