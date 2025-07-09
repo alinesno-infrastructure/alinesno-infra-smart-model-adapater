4c65c6dfdf5310b3221482293e2e9c5b2162c9d1
赵阳(Yang) <yangAip@gmail.com>
2025-07-09 11:56:05 +0800

    docs:添加部署脚本 deploy.sh，用于构建和上传文档到OSS

---
+++ docs/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# copy changes.md
+cp ../changes.md ./zh/
+cp ../changes.md ./
+
+# build
+npm run docs:build
+#vuepress build .
+
+ossutil rm oss://agentsflex-docs/ -rf
+ossutil cp -rf assets/images  oss://agentsflex-docs/assets/images
+ossutil cp -rf .vitepress/dist  oss://agentsflex-docs/
