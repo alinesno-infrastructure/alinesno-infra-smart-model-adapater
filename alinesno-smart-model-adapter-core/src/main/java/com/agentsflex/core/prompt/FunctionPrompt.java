06e53f9da1e0e87da72e580566702ffdc6c25338
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:07 +0800

    feat:创建FunctionPrompt类，用于管理系统消息和用户消息

---
+++ alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/prompt/FunctionPrompt.java
@@ -0,0 +1,52 @@
+package com.agentsflex.core.prompt;
+
+import com.agentsflex.core.functions.Function;
+import com.agentsflex.core.message.HumanMessage;
+import com.agentsflex.core.message.Message;
+import com.agentsflex.core.message.SystemMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunctionPrompt extends Prompt {
+
+    private SystemMessage systemMessage;
+    private HumanMessage humanMessage;
+
+    public FunctionPrompt(String message, Class<?> functionsClass) {
+        this.humanMessage = new HumanMessage(message);
+        this.humanMessage.addFunctions(functionsClass);
+    }
+
+    public FunctionPrompt(String message, List<Function> functions) {
+        this.humanMessage = new HumanMessage(message);
+        this.humanMessage.addFunctions(functions);
+    }
+
+    public SystemMessage getSystemMessage() {
+        return systemMessage;
+    }
+
+    public void setSystemMessage(SystemMessage systemMessage) {
+        this.systemMessage = systemMessage;
+    }
+
+    public HumanMessage getHumanMessage() {
+        return humanMessage;
+    }
+
+    public void setHumanMessage(HumanMessage humanMessage) {
+        this.humanMessage = humanMessage;
+    }
+
+    @Override
+    public List<Message> toMessages() {
+        List<Message> messages = new ArrayList<>();
+        if (systemMessage != null){
+            messages.add(0, systemMessage);
+        }
+
+        messages.add(humanMessage);
+        return messages;
+    }
+}
