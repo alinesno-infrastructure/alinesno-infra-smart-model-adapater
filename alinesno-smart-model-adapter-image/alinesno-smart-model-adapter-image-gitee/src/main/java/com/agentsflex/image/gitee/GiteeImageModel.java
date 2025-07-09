76a51b09aba645f6c8619adbb23606bd6eb3a84f
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:07 +0800

    feat: 创建GiteeImageModel类，实现图像生成功能。

---
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-gitee/src/main/java/com/agentsflex/image/gitee/GiteeImageModel.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright (c) 2023-2025, alinesno-smart-model-adapter (fuhai999@gmail.com).
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.agentsflex.image.gitee;
+
+import com.agentsflex.core.image.*;
+import com.agentsflex.core.llm.client.HttpClient;
+import com.agentsflex.core.util.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GiteeImageModel implements ImageModel {
+
+    private final GiteeImageModelConfig config;
+    private final HttpClient httpClient = new HttpClient();
+
+    public GiteeImageModel(GiteeImageModelConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public ImageResponse generate(GenerateImageRequest request) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Authorization", "Bearer " + config.getApiKey());
+
+        String payload = Maps.of("inputs", request.getPrompt())
+            .setIfNotNull("width", request.getWidth())
+            .setIfNotNull("height", request.getHeight())
+            .toJSON();
+
+        String url = config.getEndpoint() + "/api/serverless/" + config.getModel() + "/text-to-image";
+        byte[] imageBytes = httpClient.postBytes(url, headers, payload);
+        if (imageBytes == null || imageBytes.length == 0) {
+            return ImageResponse.error("can not read the image bytes.");
+        }
+
+        ImageResponse response = new ImageResponse();
+        response.addImage(imageBytes);
+
+        return response;
+    }
+
+
+    @Override
+    public ImageResponse edit(EditImageRequest request) {
+        throw new IllegalStateException("GiteeImageModel Can not support edit image.");
+    }
+
+    @Override
+    public ImageResponse vary(VaryImageRequest request) {
+        throw new IllegalStateException("GiteeImageModel Can not support vary image.");
+    }
+
+}
