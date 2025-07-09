2bd2bca55922011533abe1df64cace9f7e55e5f7
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:06 +0800

    feat:创建AlgorithmBaseResp类，包含状态消息和状态码属性

---
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-doubao/src/main/java/com/agentsflex/image/doubao/bean/AlgorithmBaseResp.java
@@ -0,0 +1,10 @@
+package com.agentsflex.image.doubao.bean;
+
+import lombok.Data;
+
+// 算法基础响应对象
+@Data
+public class AlgorithmBaseResp {
+    private String status_message;
+    private int status_code;
+}
