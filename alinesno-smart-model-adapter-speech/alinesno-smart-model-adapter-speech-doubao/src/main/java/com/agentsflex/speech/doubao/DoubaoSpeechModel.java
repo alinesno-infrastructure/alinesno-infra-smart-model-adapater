48a4ec9f8a937cf3283519f83ad51f207f0e4743
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:05 +0800

    feat:创建Doubao语音识别接口类DoubaoSpeechModel

---
+++ alinesno-smart-model-adapter-speech/alinesno-smart-model-adapter-speech-doubao/src/main/java/com/agentsflex/speech/doubao/DoubaoSpeechModel.java
@@ -0,0 +1,20 @@
+package com.agentsflex.speech.doubao;
+
+import com.agentsflex.core.speech.*;
+
+import java.util.List;
+
+/**
+ * Doubao语音识别接口
+ */
+public class DoubaoSpeechModel implements SpeechModel {
+    @Override
+    public SpeechResponse synthesize(SynthesizeSpeechRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<RecognizeSpeechResponse> recognize(RecognizeSpeechRequest request) {
+        return null;
+    }
+}
