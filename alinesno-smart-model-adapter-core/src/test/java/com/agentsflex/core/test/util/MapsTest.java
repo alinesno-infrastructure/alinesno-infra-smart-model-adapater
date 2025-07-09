42dbad83768a146358b95f92ac7ff143768b8894
梁明树(Nina) <ninaAip@gmail.com>
2025-07-09 11:56:08 +0800

    test:添加Maps工具类测试用例

---
+++ alinesno-smart-model-adapter-core/src/test/java/com/agentsflex/core/test/util/MapsTest.java
@@ -0,0 +1,19 @@
+package com.agentsflex.core.test.util;
+
+import com.agentsflex.core.util.Maps;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class MapsTest {
+
+    @Test
+    public void testMaps() {
+        Map<String, Object> map1 = Maps.of("key", "value")
+            .setChild("options.aaa", 1);
+
+        Assert.assertEquals(1, ((Map<?, ?>) map1.get("options")).get("aaa"));
+        System.out.println(map1);
+    }
+}
