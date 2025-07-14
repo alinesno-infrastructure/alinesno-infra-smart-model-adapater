344e757c0b00809f2ccbe2d6285b6c7877abcfd1
李伟(Michael) <michaelAip@gmail.com>
2025-07-15 00:18:02 +0800

    test:添加ChainNodeGroovy测试类，用于验证链式执行流程

---
+++ alinesno-smart-model-adapter-chain/alinesno-smart-model-adapter-chain-groovy/src/test/java/com/agentsflex/chain/node/ChainNodeGroovyTest.java
@@ -0,0 +1,51 @@
+package com.agentsflex.chain.node;
+
+import com.agentsflex.core.chain.Chain;
+import com.agentsflex.core.chain.ChainEdge;
+import com.agentsflex.core.chain.node.EndNode;
+import com.agentsflex.core.chain.node.StartNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChainNodeGroovyTest {
+
+    public static void main(String[] args) {
+        // 创建执行链
+        Chain chain = new Chain();
+
+        // 创建开始节点
+        StartNode startNode = new StartNode();
+        startNode.setId("1");
+        chain.addNode(startNode);
+
+        // 创建中间节点（动态代码节点）
+        GroovyExecNode groovyExecNode = new GroovyExecNode();
+        groovyExecNode.setId("2");
+        groovyExecNode.setCode("System.out.println('groovy hello world')");
+        chain.addNode(groovyExecNode);
+
+        // 创建结束节点
+        EndNode endNode = new EndNode();
+        endNode.setId("3");
+        endNode.setMessage("success");
+        chain.addNode(endNode);
+
+        // 创建 1-2 的边
+        ChainEdge edge12 = new ChainEdge();
+        edge12.setSource("1");
+        edge12.setTarget("2");
+        chain.addEdge(edge12);
+
+        // 创建 2-3 的边
+        ChainEdge edge23 = new ChainEdge();
+        edge23.setSource("2");
+        edge23.setTarget("3");
+        chain.addEdge(edge23);
+
+        // 执行执行链
+        Map<String, Object> result = chain.executeForResult(new HashMap<>());
+        System.out.println(result);
+    }
+
+}
