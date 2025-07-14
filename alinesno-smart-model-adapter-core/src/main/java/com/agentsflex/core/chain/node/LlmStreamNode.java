ebcd03ef235df86d5aad89fc32f41488e5aa2746
李伟(Michael) <michaelAip@gmail.com>
2025-07-15 00:18:02 +0800

    feat:新增LlmStreamNode类，用于处理流式LLM调用逻辑

---
+++ alinesno-smart-model-adapter-core/src/main/java/com/agentsflex/core/chain/node/LlmStreamNode.java
@@ -0,0 +1,203 @@
+package com.agentsflex.core.chain.node;
+
+import com.agentsflex.core.chain.Chain;
+import com.agentsflex.core.chain.DataType;
+import com.agentsflex.core.chain.Parameter;
+import com.agentsflex.core.llm.ChatOptions;
+import com.agentsflex.core.llm.Llm;
+import com.agentsflex.core.llm.StreamResponseListener;
+import com.agentsflex.core.message.SystemMessage;
+import com.agentsflex.core.prompt.TextPrompt;
+import com.agentsflex.core.prompt.template.TextPromptTemplate;
+import com.agentsflex.core.util.Maps;
+import com.agentsflex.core.util.StringUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+@Getter
+public class LlmStreamNode extends BaseNode {
+
+    protected Llm llm;
+    protected ChatOptions chatOptions = ChatOptions.DEFAULT;
+    protected StreamResponseListener listener;
+    protected String userPrompt;
+    protected TextPromptTemplate userPromptTemplate;
+
+    protected String systemPrompt;
+    protected TextPromptTemplate systemPromptTemplate;
+    protected String outType = "text"; //text markdown json
+
+    public LlmStreamNode() {
+    }
+
+
+    public LlmStreamNode(Llm llm, String userPrompt) {
+        this.llm = llm;
+        this.userPrompt = userPrompt;
+        this.userPromptTemplate = StringUtil.hasText(userPrompt)
+            ? TextPromptTemplate.of(userPrompt) : null;
+    }
+
+
+    public void setLlm(Llm llm) {
+        this.llm = llm;
+    }
+
+    public void setUserPrompt(String userPrompt) {
+        this.userPrompt = userPrompt;
+        this.userPromptTemplate = StringUtil.hasText(userPrompt)
+            ? TextPromptTemplate.of(userPrompt) : null;
+    }
+
+    public void setSystemPrompt(String systemPrompt) {
+        this.systemPrompt = systemPrompt;
+        this.systemPromptTemplate = StringUtil.hasText(systemPrompt)
+            ? TextPromptTemplate.of(systemPrompt) : null;
+    }
+
+    public void setChatOptions(ChatOptions chatOptions) {
+        if (chatOptions == null) {
+            chatOptions = ChatOptions.DEFAULT;
+        }
+        this.chatOptions = chatOptions;
+    }
+
+    public void setOutType(String outType) {
+        this.outType = outType;
+    }
+
+    @Override
+    protected Map<String, Object> execute(Chain chain) {
+        Map<String, Object> parameterValues = chain.getParameterValues(this);
+
+        if (userPromptTemplate == null) {
+            return Collections.emptyMap();
+        }
+
+        TextPrompt userPrompt = userPromptTemplate.format(parameterValues);
+
+        if (systemPromptTemplate != null) {
+            String systemPrompt = systemPromptTemplate.formatToString(parameterValues);
+            userPrompt.setSystemMessage(SystemMessage.of(systemPrompt));
+        }
+
+        // 创建一个 CompletableFuture 用于异步通知
+        CompletableFuture<String> future = new CompletableFuture<>();
+        llm.chatStream(userPrompt, listener, chatOptions);
+        // 同步节点等待 future 完成
+        try {
+            String resultOutput = future.get();
+            System.out.println("resultOutput= " + resultOutput);
+            return Maps.of("output", resultOutput);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static Object getOutputDefData(Parameter output, JSONObject data, boolean sub) {
+        String name = output.getName();
+        DataType dataType = output.getDataType();
+        switch (dataType) {
+            case Array, Array_Object -> {
+                if (output.getChildren() == null || output.getChildren().isEmpty()) {
+                    return data.get(name);
+                }
+                List<Object> subResultList = new ArrayList<>();
+                Object dataObj = data.get(name);
+                if (dataObj instanceof JSONArray contentFields) {
+                    if (!contentFields.isEmpty()) {
+                        contentFields.forEach(field -> {
+                            if (field instanceof JSONObject) {
+                                subResultList.add(getChildrenResult(output.getChildren(), (JSONObject) field, sub));
+                            }
+                        });
+                    }
+                }
+                return subResultList;
+            }
+            case Object -> {
+                return (output.getChildren() != null && !output.getChildren().isEmpty()) ? getChildrenResult(output.getChildren(), sub ? data : (JSONObject) data.get(name), sub) : data.get(name);
+            }
+            case String, Number, Boolean -> {
+                Object obj = data.get(name);
+                return (DataType.String == dataType) ? (obj instanceof String ? obj : "") : (DataType.Number == dataType) ? (obj instanceof Number ? obj : 0) : obj instanceof Boolean ? obj : false;
+            }
+            case Array_String, Array_Number, Array_Boolean -> {
+                Object arrayObj = data.get(name);
+                if (arrayObj instanceof JSONArray) {
+                    ((JSONArray) arrayObj).removeIf(o -> arrayRemoveFlag(dataType, o));
+                    return arrayObj;
+                }
+                return Collections.emptyList();
+            }
+            default -> {
+                return ""; // FILE和其他不支持的类型，默认空字符串
+            }
+        }
+    }
+
+    private static boolean arrayRemoveFlag(DataType dataType, Object arrayObj) {
+        boolean removeFlag = false;
+        if (DataType.Array_String == dataType) {
+            if (!(arrayObj instanceof String)) {
+                removeFlag = true;
+            }
+        } else if (DataType.Array_Number == dataType) {
+            if (!(arrayObj instanceof Number)) {
+                removeFlag = true;
+            }
+        } else {
+            if (!(arrayObj instanceof Boolean)) {
+                removeFlag = true;
+            }
+        }
+        return removeFlag;
+    }
+
+    private static Map<String, Object> getChildrenResult(List<Parameter> children, JSONObject data, boolean sub) {
+        Map<String, Object> childrenResult = new HashMap<>();
+        children.forEach(child -> {
+            String childName = child.getName();
+            Object subData = getOutputDefData(child, data, sub);
+            if ((subData instanceof JSONObject) && (child.getChildren() != null && !child.getChildren().isEmpty())) {
+                getChildrenResult(child.getChildren(), (JSONObject) subData, true);
+            } else {
+                childrenResult.put(childName, subData);
+            }
+        });
+        return childrenResult;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LlmNode{" +
+            "llm=" + llm +
+            ", chatOptions=" + chatOptions +
+            ", userPrompt='" + userPrompt + '\'' +
+            ", userPromptTemplate=" + userPromptTemplate +
+            ", systemPrompt='" + systemPrompt + '\'' +
+            ", systemPromptTemplate=" + systemPromptTemplate +
+            ", outType='" + outType + '\'' +
+            ", description='" + description + '\'' +
+            ", parameters=" + parameters +
+            ", outputDefs=" + outputDefs +
+            ", id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            ", async=" + async +
+            ", inwardEdges=" + inwardEdges +
+            ", outwardEdges=" + outwardEdges +
+            ", condition=" + condition +
+            ", memory=" + memory +
+            ", nodeStatus=" + nodeStatus +
+            '}';
+    }
+}
