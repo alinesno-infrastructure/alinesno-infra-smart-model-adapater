2b3ab34965c0c26f0bd649258e0778fef9dcdd50
张婷(Tina) <tinaAip@gmail.com>
2025-09-13 09:20:49 +0800

    fix:修改API密钥配置和图片保存路径，调整提示词内容

--- alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-doubao/src/test/java/com/agentsflex/image/doubao/test/DoubaoImageModelTest.java
+++ alinesno-smart-model-adapter-image/alinesno-smart-model-adapter-image-doubao/src/test/java/com/agentsflex/image/doubao/test/DoubaoImageModelTest.java
@@ -17,17 +17,16 @@ public class DoubaoImageModelTest {
     @Test
     public void testGenImage(){
         DoubaoImageModelConfig config = new DoubaoImageModelConfig();
-        config.setAccessKey(System.getenv("ALINESNO_DOUBAO_API_KEY"));
-        config.setSecretKey(System.getenv("ALINESNO_DOUBAO_API_SECRET"));
+        config.setApiKey(System.getenv("ALINESNO_DOUBAO_GPT_API_KEY"));
 
         System.out.println("config = " + config) ;
 
         DoubaoImageModel openAiImageModel = new DoubaoImageModel(config);
 
         GenerateImageRequest request = new GenerateImageRequest();
-        request.setPrompt("画面左侧是一个人皱着眉头，有些生气的表情，旁边气泡中文字：“有人说我说话直别介意怎么办？”，字体为黑色描边的白色字体。这个人站在一个普通的室内场景中，身后有简单的沙发和茶几。 画面右侧是另一个人双手抱在胸前，一脸不屑，旁边气泡中文字：“我怼人你也别介意。”，字体为红色立体的抖音风格字体。这个人站在同样的室内场景中，但位置与左侧的人稍有间隔，身后也有简单的沙发和茶几。整个场景色调较为明亮，但两个人的情绪让画面稍显紧张。") ;
+//        request.setPrompt("画面左侧是一个人皱着眉头，有些生气的表情，旁边气泡中文字：“有人说我说话直别介意怎么办？”，字体为黑色描边的白色字体。这个人站在一个普通的室内场景中，身后有简单的沙发和茶几。 画面右侧是另一个人双手抱在胸前，一脸不屑，旁边气泡中文字：“我怼人你也别介意。”，字体为红色立体的抖音风格字体。这个人站在同样的室内场景中，但位置与左侧的人稍有间隔，身后也有简单的沙发和茶几。整个场景色调较为明亮，但两个人的情绪让画面稍显紧张。") ;
+        request.setPrompt("城市治理微服务架构图") ; //画面左侧是一个人皱着眉头，有些生气的表情，旁边气泡中文字：“有人说我说话直别介意怎么办？”，字体为黑色描边的白色字体。这个人站在一个普通的室内场景中，身后有简单的沙发和茶几。 画面右侧是另一个人双手抱在胸前，一脸不屑，旁边气泡中文字：“我怼人你也别介意。”，字体为红色立体的抖音风格字体。这个人站在同样的室内场景中，但位置与左侧的人稍有间隔，身后也有简单的沙发和茶几。整个场景色调较为明亮，但两个人的情绪让画面稍显紧张。") ;
         request.setNegativePrompt("人物");
-
         request.setN(3);
 
         request.setSize(512,512);
@@ -35,7 +34,7 @@ public class DoubaoImageModelTest {
         if (generate != null && generate.getImages() != null){
             int index = 0;
             for (Image image : generate.getImages()) {
-                image.writeToFile(new File("E:\\tmp\\mp3\\image_" + UUID.randomUUID() + "_" +(index++)+".jpg"));
+                image.writeToFile(new File("/Users/luodong/Desktop/demo/image_" + UUID.randomUUID() + "_" +(index++)+".jpg"));
             }
         }
 
